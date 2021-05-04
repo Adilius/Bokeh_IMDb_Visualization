b599353705a25af2112ba28c875e23bf972d6a1c
Adilius <leugue-gamer@hotmail.com>
2021-05-05 01:27:48 +0200

    Update

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
 from bokeh.models import ColumnDataSource
 import pandas as pd
 
-print("Hello world")
+df = pd.read_csv('imdb_movie_metadata.csv', encoding="utf8")
+df.dropna(inplace=True)  # Drop rows with missing attributes
+df.drop_duplicates(inplace=True) # Remove duplicates
+df.reset_index(drop=True ,inplace=True)  # Reset index
+df.sort_index(axis=1, inplace=True)  #Sort by name
 
-csv_dataframe = pd.read_csv('imdb_movie_metadata.csv', encoding="utf8")
-csv_dataframe.dropna(inplace=True)  # Drop rows with missing attributes
-csv_dataframe.drop_duplicates(inplace=True) # Remove duplicates
-csv_dataframe.reset_index(drop=True ,inplace=True)  # Reset index
+df_group = df['color'].value_counts()
+print(df_group)
 
-print(csv_dataframe)
+print(df)
 
-Columns = [TableColumn(field=Ci, title=Ci) for Ci in csv_dataframe.columns]
-data_table = DataTable(autosize_mode='fit_viewport', columns=Columns, source=ColumnDataSource(csv_dataframe))
+Columns = [TableColumn(field=Ci, title=Ci) for Ci in df.columns]
+source = ColumnDataSource(df)
+data_table = DataTable(autosize_mode='fit_viewport', columns=Columns, source=source)
 
 show(data_table)
